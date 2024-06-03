22ae667e9c592ae3ec9aaa3e5e7af3f5f847412f
Mithu1424882 <biswasmithumcb@gmail.com>
2024-06-03 23:20:37 +0600

    create add test page and design add test form data

--- src/components/Form/AddTestForm.jsx
+++ src/components/Form/AddTestForm.jsx
@@ -1,81 +1,76 @@
-// import { categories } from '../Categories/CategoriesData'
 
-const AddTestForm = () => {
+import { DateRange } from 'react-date-range'
+import { TbFidgetSpinner } from 'react-icons/tb'
+const AddTestForm = ({
+  dates,
+  handleDates,
+  handleSubmit,
+  setImagePreview,
+  imagePreview,
+  imageText,
+  handleImage,
+  loading,
+}) => {
   return (
-    <div className='w-full min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50'>
-      <form>
+    <div className='w-11/12 mx-auto min-h-[calc(100vh-40px)] flex flex-col justify-center items-center text-gray-800 rounded-xl bg-gray-50 p-4'>
+      <form onSubmit={handleSubmit}>
         <div className='grid grid-cols-1 lg:grid-cols-2 gap-10'>
           <div className='space-y-6'>
             <div className='space-y-1 text-sm'>
               <label htmlFor='location' className='block text-gray-600'>
-                Location
+                Test Name
               </label>
               <input
-                className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
-                name='location'
-                id='location'
+                className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md input-sm'
+                name='test_name'
+                id='test_name'
                 type='text'
-                placeholder='Location'
+                placeholder='Test Name...'
                 required
               />
             </div>
-
-            {/* <div className='space-y-1 text-sm'>
-              <label htmlFor='category' className='block text-gray-600'>
-                Category
-              </label>
-              <select
-                required
-                className='w-full px-4 py-3 border-rose-300 focus:outline-rose-500 rounded-md'
-                name='category'
-              >
-                {categories.map(category => (
-                  <option value={category.label} key={category.label}>
-                    {category.label}
-                  </option>
-                ))}
-              </select>
-            </div> */}
 
             <div className='space-y-1'>
               <label htmlFor='location' className='block text-gray-600'>
-                Select Availability Range
+                Select Date
               </label>
               {/* Calender */}
+              <DateRange
+                rangeColors={['#F43F5E']}
+                editableDateInputs={true}
+                onChange={item => handleDates(item)}
+                moveRangeOnFirstSelection={false}
+                ranges={[dates]}
+              />
             </div>
           </div>
           <div className='space-y-6'>
-            <div className='space-y-1 text-sm'>
-              <label htmlFor='title' className='block text-gray-600'>
-                Title
-              </label>
-              <input
-                className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
-                name='title'
-                id='title'
-                type='text'
-                placeholder='Title'
-                required
-              />
-            </div>
-
-            <div className=' p-4 bg-white w-full  m-auto rounded-lg'>
+            <div className=' p-2 bg-white w-full  m-auto rounded-lg flex justify-between items-center'>
               <div className='file_upload px-5 py-3 relative border-4 border-dotted border-gray-300 rounded-lg'>
                 <div className='flex flex-col w-max mx-auto text-center'>
                   <label>
                     <input
                       className='text-sm cursor-pointer w-36 hidden'
                       type='file'
+                      onChange={e => handleImage(e.target.files[0])}
                       name='image'
                       id='image'
                       accept='image/*'
                       hidden
                     />
                     <div className='bg-rose-500 text-white border border-gray-300 rounded font-semibold cursor-pointer p-1 px-3 hover:bg-rose-500'>
-                      Upload Image
+                      {/* {imageText} */}
+                      {imageText.length > 20
+                        ? imageText.split('.')[0].slice(0, 15) +
+                        '....' +
+                        imageText.split('.')[1]
+                        : imageText}
                     </div>
                   </label>
                 </div>
+              </div>
+              <div className='h-16 w-16 object-cover overflow-hidden flex items-center'>
+                {imagePreview && <img src={imagePreview} />}
               </div>
             </div>
             <div className='flex justify-between gap-2'>
@@ -84,7 +79,7 @@ const AddTestForm = () => {
                   Price
                 </label>
                 <input
-                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
+                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md input-sm'
                   name='price'
                   id='price'
                   type='number'
@@ -95,68 +90,42 @@ const AddTestForm = () => {
 
               <div className='space-y-1 text-sm'>
                 <label htmlFor='guest' className='block text-gray-600'>
-                  Total guest
+                  Total Slots
                 </label>
                 <input
-                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
-                  name='total_guest'
-                  id='guest'
+                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md input-sm'
+                  name='total_slots'
+                  id='total_slots'
                   type='number'
-                  placeholder='Total guest'
+                  placeholder='Total Slots'
                   required
                 />
               </div>
             </div>
-
-            <div className='flex justify-between gap-2'>
-              <div className='space-y-1 text-sm'>
-                <label htmlFor='bedrooms' className='block text-gray-600'>
-                  Bedrooms
-                </label>
-                <input
-                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
-                  name='bedrooms'
-                  id='bedrooms'
-                  type='number'
-                  placeholder='Bedrooms'
-                  required
-                />
-              </div>
-
-              <div className='space-y-1 text-sm'>
-                <label htmlFor='bathrooms' className='block text-gray-600'>
-                  Bathrooms
-                </label>
-                <input
-                  className='w-full px-4 py-3 text-gray-800 border border-rose-300 focus:outline-rose-500 rounded-md '
-                  name='bathrooms'
-                  id='bathrooms'
-                  type='number'
-                  placeholder='Bathrooms'
-                  required
-                />
-              </div>
-            </div>
-
             <div className='space-y-1 text-sm'>
               <label htmlFor='description' className='block text-gray-600'>
-                Description
+                Details
               </label>
 
               <textarea
-                id='description'
-                className='block rounded-md focus:rose-300 w-full h-32 px-4 py-3 text-gray-800  border border-rose-300 focus:outline-rose-500 '
-                name='description'
+                id='details'
+                className='block rounded-md focus:rose-300 w-full h-20 px-4 py-3 text-gray-800  border border-rose-300 focus:outline-rose-500 '
+                name='details'
               ></textarea>
             </div>
           </div>
         </div>
 
         <button
+          disabled={loading}
           type='submit'
           className='w-full p-3 mt-5 text-center font-medium text-white transition duration-200 rounded shadow-md bg-rose-500'
         >
-          Save & Continue
+          {loading ? (
+            <TbFidgetSpinner className='animate-spin m-auto' />
+          ) : (
+            ' Add Test'
+          )}
         </button>
       </form>
     </div>
